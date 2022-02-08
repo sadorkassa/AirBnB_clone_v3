99c58d8ead8db6c4e892d89c7c7b8b499f09354c
Sador Kassa <sadorkassa21@gmail.com>
2022-02-08 06:21:49 +0000

    AirBnB_clone_v3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:         display "Hello HBNB!"
+            /hbnb:     display "HBNB"
+            /c/<text>: display "C" + text (replace underscores with space)
+"""
+
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+    """display text"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
+    """display text"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
